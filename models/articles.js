116a01cacb2154877fa5c9b9979484512840b55a
Trilok Lowanshi <trilokl01c@gmail.com>
2026-02-27 13:05:56 +0530

    watched old work 1

--- models/articles.js
+++ models/articles.js
@@ -1,7 +1,7 @@
 // importing mongoose
 const mongoose = require("mongoose");
 
-// defining schema, a blueprint for the data base
+// defining schema, a blueprint for the database
 const articleSchema = new mongoose.Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
